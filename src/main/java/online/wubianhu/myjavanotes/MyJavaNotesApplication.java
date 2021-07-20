d0314de69684fff896d4913ecbabe8008ddf66b6
wubianhu1996 <wubianhu1996@gmail.com>
2021-07-20 14:23:58 +0800

    first commit

---
+++ src/main/java/online/wubianhu/myjavanotes/MyJavaNotesApplication.java
@@ -0,0 +1,13 @@
+package online.wubianhu.myjavanotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJavaNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/wubianhu/myjavanotes/MyJavaNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package online.wubianhu.myjavanotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
